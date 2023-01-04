d5bd4dd2a65d6bf1f44c7dbd0446b150c255c61c
U003178 <111997488+U003178@users.noreply.github.com>
2023-01-04 17:34:43 -0300

    Alteraçaõ

--- Controllers/OperatorsController.cs
+++ Controllers/OperatorsController.cs
@@ -4,16 +4,16 @@ namespace IntegracaoTeste_Gustavo2.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class OperatorsController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<OperatorsController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public OperatorsController(ILogger<OperatorsController> logger)
     {
         _logger = logger;
     }
