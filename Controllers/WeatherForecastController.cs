31bf72cb95c66c3c2f93861eb489b7e2b5bab424
U003178 <111997488+U003178@users.noreply.github.com>
2023-01-04 16:48:50 -0300

    TesteV1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,15 +18,16 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet]
+    [Route("multiplicar")]
+    public double multiplicar(double n1, double n2)
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return n1 * n2;
+    }
+    [HttpGet]
+    [Route("divisao")]
+    public double divisao(double n1, double n2)
+    {
+        return n1 / n2;
     }
 }
